e210162b582b088802681b19c3b25689538c1699
alanzarli <105911064+alanzarli@users.noreply.github.com>
2023-09-19 16:12:04 +0200

    feat: test img

--- src/components/Mcd.jsx
+++ src/components/Mcd.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import exempleMCD from '../assets/exemple-MCD.jpg'
 
 const Mcd = () => {
   return (
-    <div>
-      <h1>TEST</h1>
+    <div className='w-100 m-auto'>
+      <div className="m-auto"> 
+        <h1>TEST</h1>
+        <img  className="img-diag"src={exempleMCD} alt="exemple_MCD" />
+      </div>
     </div>
   )
 }
