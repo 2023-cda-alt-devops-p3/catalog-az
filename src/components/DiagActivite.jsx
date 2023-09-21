794f49b97c4c45f48cd663f4f22bf08306cc1628
alanzarli <105911064+alanzarli@users.noreply.github.com>
2023-09-21 10:55:10 +0200

    add: text activity diag

--- src/components/DiagActivite.jsx
+++ src/components/DiagActivite.jsx
@@ -8,7 +8,19 @@ const DiagActivite = () => {
         <h1 className="text-center mt-10" >Diagramme UML d'Activités</h1>
         <div className='mt-50'>
           <img  className="img-diag"src={exempleDA} alt="exemple_Diag_Activite" />
-          <p className='mt-10'>Un diagramme d'activité fournit une vue du comportement d'un système en décrivant la séquence d'actions d'un processus. Les diagrammes d'activité sont similaires aux organigrammes de traitement de l'information, car ils montrent les flux entre les actions dans une activité.</p>
+          <p className='mt-10'>
+Le diagramme d’activité (Activity Diagram) fait parti des diagrammes comportementaux. Il est utilisé pour modéliser les aspects dynamiques d'un système. Il s'agit de représenter les opérations d'un processus et leurs conséquences sur les objets (logiciels ou matériels). La modélisation peut être utilisée pour décrire le déroulement d'un cas
+d'utilisation ou d'une méthode.
+<br />
+Les diagrammes d'activité affichent le flux de travail d'un point de départ à un point d'arrivée en détaillant les nombreux chemins de décision existant dans la progression des événements contenus dans l'activité.
+Ils peuvent être utilisés pour détailler des situations dans lesquelles un traitement parallèle peut avoir lieu lors de l'exécution de certaines activités. Les diagrammes d'activités sont utiles pour la modélisation métier car ils sont utilisés pour détailler les processus impliqués dans les activités métier.
+
+<br />
+Les diagrammes d'activités permettent de mettre l'accent sur les traitements. Les diagrammes d'activités ne sont pas spécifiquement rattachés à un classeur particulier.
+Nous pouvons attacher un diagramme d'activités à n'importe quel élément de modélisation afin de visualiser, spécifier, construire ou documenter le comportement de cet élément.
+Dans la phase de conception, les diagrammes d'activités sont particulièrement adaptés pour une plus grande description des cas d'utilisation. Plus précisément, ils viennent illustrer et consolider la description textuelle.
+De plus, leur représentation sous forme d'organigrammes nous les rend facilement intelligibles. Nous nous concentrons ici sur les activités telles que les voient les acteurs qui collaborent avec le système dans le cadre d'un processus métier. La modélisation du flot d'objets est souvent importante dans ce type d'utilisation des diagrammes d'activités.
+</p>
         </div>
        
       </div>
