475dcc6d26efe36a609f1043f5d7c49e534191d1
alanzarli <105911064+alanzarli@users.noreply.github.com>
2023-09-21 14:10:07 +0200

    add: content

--- src/components/DiagEtat.jsx
+++ src/components/DiagEtat.jsx
@@ -11,6 +11,10 @@ const DiagEtat = () => {
         <img  className="img-diag"src={exempleDE} alt="exemple_Diag_Activite" />
         <p className='mt-10'>Représenter des objets liés à un événement dans un système réactif. Illustrer des cas d'utilisation dans un contexte d'entreprise. Décrire comment un objet change d'état au cours de son existence.
 </p>
+<p className='mt-10'>
+  Le diagramme états-transitions (State Machine Diagram ou Statechart Diagram) fait parti des diagrammes comportementaux. Son rôle, est de décrire le fonctionnement d’une machine (ou d’un objet) ayant un comportement séquentiel.
+  Il représente les différents états (situations) dans lesquels peut se trouver la machine (ou l’objet) ainsi que la façon dont cette machine (ou l'objet) passe d’un état à l’autre en réponse à des événements.
+</p>
       </div>
      
     </div>
