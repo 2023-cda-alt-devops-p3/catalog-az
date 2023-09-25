1890ee742db6b96171541832f4442a2607a6a58f
alanzarli <105911064+alanzarli@users.noreply.github.com>
2023-09-25 14:05:06 +0200

    fix: card txt

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -6,13 +6,12 @@ const Overview = () => {
   return (
     <div className="card-container mt-50">
           {
-            CardInfos.map(({id, title, img, url, spec}) => (
+            CardInfos.map(({id, title, img, url, spec, cardtxt}) => (
                     <div className="cards" key={id}>
                         <img src={img} alt={`image_diagram_${id}`} className="model-picture" />
                         <h2 className="type">{spec}</h2>
                         <h1 className="card-title">{title}</h1>
-                        <p className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit distinctio rem,
-                            architecto veniam deleniti eligendi? Debitis iure voluptate nobis fugit.</p>
+                        <p className="description">{cardtxt}</p>
                         <div className="shopping-container">
                             <NavLink to={url}><button className="basket-button">VOIR PLUS</button></NavLink>
                         </div>
