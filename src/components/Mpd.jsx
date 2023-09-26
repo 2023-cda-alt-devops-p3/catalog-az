1b34b5f556ffe7b63c21a78530cc146fd8e3024e
alanzarli <105911064+alanzarli@users.noreply.github.com>
2023-09-26 10:34:10 +0200

    fix: css errors

--- src/components/Mpd.jsx
+++ src/components/Mpd.jsx
@@ -11,6 +11,7 @@ const Mpd = () => {
         <img  className="img-diag"src={exempleMPD} alt="exemple_Diag_Activite" />
         <div className='mt-10 text-info'>
           <p>Dans la méthode Merise, le modèle physique des données (MPD) consiste à implanter une base de données dans un SGBDR. Le langage utilisé pour ce type d'opération est le SQL.</p>
+          <br />
           <p>Concrètement, cette étape permet de construire la structure finale de la base de données avec les différents liens entre les éléments qui la composent. Pour la peine, on change aussi de vocabulaire </p>
             <ul>
               <li>Les entités se transforment en tables ;</li>
