d262e5d371d579d6fd02e65da577c6b6ca24453c
alanzarli <105911064+alanzarli@users.noreply.github.com>
2023-09-28 12:16:16 +0200

    fix: correction

--- src/components/ButtonTopi.jsx
+++ src/components/ButtonTopi.jsx
@@ -9,7 +9,7 @@ const ButtonTopi = () => {
   return (
 
     <div >
-        <AiOutlineArrowUp  onClick={scrollTop} size={60} className="flex justify-between items-center w-40 h-14 px-4 top-90 right-5 fixed black rond" />
+        <AiOutlineArrowUp  onClick={scrollTop} size={60} className="flex justify-between items-center w-40 h-14 px-4 top-90 right-2 fixed rond" />
     </div>
         
   )
